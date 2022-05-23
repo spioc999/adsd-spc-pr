c81844591c41a0f52f15c771257e586338f87c10
Simone Pio Caronia <spcaronia@gmail.com>
2022-05-23 22:07:50 +0200

    added confirm endpoint

--- SupervisorUtils.py
+++ SupervisorUtils.py
@@ -5,11 +5,23 @@ NODE_PORT = "node_port"
 STATUS = "status"
 TIME = "time"
 TREE_BRANCH_SIZE = 2 #Binary tree
+SON = "son"
 
 
 def getRegisterNodeInfo(json):
-    if all(key in json.keys() for key in [NODE_IP, NODE_PORT]):
-
+    if json and all(key in json.keys() for key in [NODE_IP, NODE_PORT]):
         return json[NODE_IP], json[NODE_PORT]
-    else:
-        print("Throws exception")
+    
+    raise Exception('Bad request', 400)
+
+
+def getConfirmNodeInfo(json):
+    if json and all(key in json.keys() for key in [FATHER, SON]):
+
+        father = json[FATHER]
+        son = json[SON]
+
+        if all(key in father.keys() for key in [NODE_IP, NODE_PORT]) and all(key in son.keys() for key in [NODE_IP, NODE_PORT]):
+            return father[NODE_IP], father[NODE_PORT], son[NODE_IP], son[NODE_PORT]
+        
+    raise Exception('Bad request', 400)
--- supervisor.py
+++ supervisor.py
@@ -15,23 +15,61 @@ def getTree():
 
 @app.route("/node/register", methods=['POST'])
 def registerNode():
-    node_ip, node_port = getRegisterNodeInfo(request.json)
-    node_id = f'{node_ip}:{node_port}'
-    father_id = None
-    if len(tree) == 0:
-        tree[node_id] = {
-            FATHER: None,
+    try:
+        node_ip, node_port = getRegisterNodeInfo(request.json)
+        node_id = f'{node_ip}:{node_port}'
+        father_id = None
+        if len(tree) == 0:
+            tree[node_id] = {
+                FATHER: None,
+                SONS: dict()
+            }
+            return '', 200
+        else:
+            for node in tree:
+                if len(tree[node][SONS]) < TREE_BRANCH_SIZE:
+                    father_id = node
+                    tree[node][SONS][node_id] = dict()
+                    tree[node][SONS][node_id][STATUS] = Status.PENDING
+                    tree[node][SONS][node_id][TIME] = time()
+                    break
+        return father_id, 200
+    except Exception as exc:
+        if not exc.args or len(exc.args) < 2:
+            return 'Error', 500
+        
+        return exc.args[0], exc.args[1]
+
+
+@app.route("/node/confirm", methods=['POST'])
+def confirmNode():
+    try:
+        father_node_ip, father_node_port, son_node_ip, son_node_port = getConfirmNodeInfo(request.json)
+        father_node_id = f'{father_node_ip}:{father_node_port}'
+        son_node_id = f'{son_node_ip}:{son_node_port}'
+        father_node = tree[father_node_id]
+        if father_node is None:
+            raise Exception('Father to register', 404) #TODO say to father to connect
+        
+        son_dict_item = father_node[SONS][son_node_id]
+        if son_dict_item is None:
+            raise Exception('Son not found', 404) #TODO father must refuse son connection
+        elif son_dict_item[STATUS] == Status.CONFIRMED:
+            return 'Already confirmed', 200
+
+        son_dict_item[STATUS] = Status.CONFIRMED
+        son_dict_item[TIME] = time()
+        tree[son_node_id] = {
+            FATHER: father_node_id,
             SONS: dict()
         }
-        return '', 200
-    else:
-        for node in tree:
-            if len(tree[node][SONS]) < TREE_BRANCH_SIZE:
-                father_id = node
-                tree[node][SONS][node_id] = dict()
-                tree[node][SONS][node_id][STATUS] = Status.PENDING
-                tree[node][SONS][node_id][TIME] = time()
-    return father_id, 200
+
+        return 'Success', 200
+    except Exception as exc:
+        if not exc.args or len(exc.args) < 2:
+            return 'Error', 500
+        
+        return exc.args[0], exc.args[1]
 
 
 if __name__ == '__main__':
