8a3532cdb8f4dedf4e2174f6fe88da9558be7666
Paolo Ruggirello <paoloruggirello@soldo.com>
2022-05-24 00:19:16 +0200

    Added method to get host address

--- SupervisorUtils.py
+++ SupervisorUtils.py
@@ -1,3 +1,4 @@
+from netifaces import interfaces, ifaddresses, AF_INET
 FATHER = "father"
 SONS = "sons"
 NODE_IP = "node_ip"
@@ -6,7 +7,7 @@ STATUS = "status"
 TIME = "time"
 TREE_BRANCH_SIZE = 2 #Binary tree
 SON = "son"
-
+host_address = None
 
 def getRegisterNodeInfo(json):
     if json and all(key in json.keys() for key in [NODE_IP, NODE_PORT]):
@@ -25,3 +26,11 @@ def getConfirmNodeInfo(json):
             return father[NODE_IP], father[NODE_PORT], son[NODE_IP], son[NODE_PORT]
         
     raise Exception('Bad request', 400)
+
+
+def get_host_address():
+    for ifaceName in interfaces():
+        addresses = ' '.join([i['addr'] for i in ifaddresses(ifaceName).setdefault(AF_INET, [{'addr':''}] )])
+        if addresses != '' and addresses != '0.0.0.0' and addresses != '127.0.0.1':
+            return addresses
+    return '127.0.0.1'
