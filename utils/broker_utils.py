ee13ad28fa24dee0a5c5d46138e29171fd742b39
Paolo Ruggirello <paoloruggirello@soldo.com>
2022-05-31 18:41:38 +0200

    fixes

--- utils/broker_utils.py
+++ utils/broker_utils.py
@@ -131,9 +131,9 @@ def handle_command_port(port_value, connection_id, current_node_ip, current_node
 
         if response.status_code != 200:
             conn.sendall(build_command(Command.RESULT, 'ERROR'))
-            print(f'I\'m {get_node_id(current_node_ip, current_node_port)} Error during confirm broker {ip_node}:{port_value}!')
+            print(f'I\'m {get_node_id(current_node_ip, current_node_port)} Error during confirm broker {get_node_id(ip_node,port_value)}!')
         else:
             update_info_connection(connection_id, is_broker=True, port=port_value)
             conn.sendall(build_command(Command.RESULT, 'OK'))
-            print(f'Broker {ip_node}:{port_value} confirmed successfully!')
+            print(f'Broker {get_node_id(ip_node,port_value)} confirmed successfully!')
         return response.status_code
